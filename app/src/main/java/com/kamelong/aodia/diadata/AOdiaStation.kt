6fe55f78640e2e18bd1cdd95b1cc0d84f7f986f2
KameLong <kamelong.dev@gmai.com>
2017-12-04 10:25:40 +0900

    12/04-10

--- app/src/main/java/com/kamelong/aodia/diadata/AOdiaStation.kt
+++ app/src/main/java/com/kamelong/aodia/diadata/AOdiaStation.kt
@@ -4,6 +4,22 @@ package com.kamelong.aodia.diadata
  */
 interface AOdiaStation {
     var name:String
+    var shortName:String
+    var mainStation:Boolean
+
+    /**
+     * 駅表示スタイルを返す
+     * 4bitの整数で表す[列車番号,番線、着時刻、発時刻]
+     */
+    fun getViewStyle(direct:Int):Int{
+        return 0
+    }
+
+    /**
+     * 駅表示スタイルのうち、発着時刻に関するbit値部分のみ返す
+     */
+    fun getTimeViewStyle():Int
+
 
 
 }
---
+++ app/src/main/java/com/kamelong/aodia/diadata/AOdiaTrain.kt
@@ -0,0 +1,22 @@
+package com.kamelong.aodia.diadata
+
+/**
+ * AOdiaで使用するTrainのInterface
+ */
+interface AOdiaTrain {
+    var name:String
+    var number:String
+    var count:String
+    var operation:String
+    var remark:String
+
+    fun getDepatureTime(station:Int):Int
+    fun getArrivalTime(station:Int):Int
+    fun getDepatureTime(station:Int,startTime: Int):Int
+    fun getArrivalTime(station:Int,startTime:Int):Int
+
+    fun getStopStyle(station:Int)
+
+
+
+}
