20ddb9e2b59bdabd59bbe84eca2d326b65691475
kamelong <kamelong.dev@gmail.com>
2023-06-03 11:00:02 +0900

    ダイヤを削除する際に警告を追加

--- app/src/main/java/com/kamelong/aodia/DiagramEditDialog.java
+++ app/src/main/java/com/kamelong/aodia/DiagramEditDialog.java
@@ -5,8 +5,11 @@ import android.content.Context;
 import android.view.View;
 import android.widget.EditText;
 
+import androidx.appcompat.app.AlertDialog;
+
 import com.kamelong.OuDia.Diagram;
 import com.kamelong.OuDia.LineFile;
+import com.kamelong.tool.SDlog;
 
 public class DiagramEditDialog extends Dialog {
     public DiagramEditDialog(final Context context, final LineFile lineFile, final Diagram diagram){
@@ -19,13 +22,28 @@ public class DiagramEditDialog extends Dialog {
             lineFile.diagram.get(lineFile.getDiagramNum()-1).name=diagram.name+"(コピー)";
         });
         findViewById(R.id.deleteDiagram).setOnClickListener(v -> {
-            lineFile.diagram.remove(diagram);
-            if(lineFile.getDiagramNum()==0){
-                lineFile.diagram.add(new Diagram(lineFile));
-            }
-            ((MainActivity)context).getAOdia().killFragment(lineFile);
-            DiagramEditDialog.this.dismiss();
+            new AlertDialog.Builder(getContext())
+                    .setTitle("警告")
+                    .setMessage("削除しますか？（保存していないデータは失われます）")
+                    .setPositiveButton("OK", (dialog, which) -> {
+                        try {
+                            lineFile.diagram.remove(diagram);
+                            if(lineFile.getDiagramNum()==0){
+                                lineFile.diagram.add(new Diagram(lineFile));
+                            }
+                            ((MainActivity)context).getAOdia().killFragment(lineFile);
+                            DiagramEditDialog.this.dismiss();
+
+                        } catch (Exception e) {
+                            SDlog.log(e);
+                        }
+                    })
+                    .setNegativeButton("Cancel", null)
+                    .show();
+
         });
+
+
         findViewById(R.id.upButton).setOnClickListener(v -> {
             int index=lineFile.diagram.indexOf(diagram);
             if(index>0){
